9593f7c171e05fcbb2348b4bf938d0231c2530e4
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-08-29 22:09:21 +0530

    add propTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
 import { PostsList } from './';
@@ -23,6 +24,10 @@ function mapStateToProps(state) {
   return {
     posts: state.posts,
   };
+}
+
+App.propTypes = {
+  posts : PropTypes.array.isRequired
 }
 
 export default connect(mapStateToProps)(App);
