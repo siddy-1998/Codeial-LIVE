ad326201366c3e977d3db8c5ba96bd3a84de8f2b
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-09-01 11:56:42 +0530

    add router 2

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import PostsList from './PostsList';
 import Navbar from './Navbar';
 import App from './App';
+import Home from './Home';
+import Page404 from './Page404'
 
-export { PostsList, App , Navbar };
+export { PostsList, App , Navbar , Home , Page404 };
