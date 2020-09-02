447e4554af2028a47b687bc515a7e282ac308688
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-09-02 23:35:54 +0530

    user login 3

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { LOGIN_START } from './actionTypes';
+import { LOGIN_START ,LOGIN_FAILED , LOGIN_SUCCESS } from './actionTypes';
 import { APIUrls } from '../helpers/urls';
 import { getFormBody } from '../helpers/utils';
 
@@ -8,8 +8,23 @@ export function startLogin() {
   };
 }
 
+export function loginFailed(errorMessage) {
+  return {
+    type: LOGIN_FAILED,
+    error: errorMessage,
+  };
+}
+
+export function loginSuccess(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    user,
+  };
+}
+
 export function login(email, password) {
   return (dispatch) => {
+    dispatch(startLogin());
     const url = APIUrls.login();
     fetch(url, {
       method: 'POST',
@@ -17,6 +32,16 @@ export function login(email, password) {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: getFormBody({ email, password }),
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data);
+        if (data.success) {
+          // dispatch action to save user
+          dispatch(loginSuccess(data.data.user));
+          return;
+        }
+        dispatch(loginFailed(data.message));
+      });
   };
 }
