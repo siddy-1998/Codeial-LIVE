71ae2f77278a397db6a3180d7cf56e1e1b7e4797
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-09-03 14:26:35 +0530

    add signup component

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,11 @@
-import { LOGIN_START ,LOGIN_FAILED , LOGIN_SUCCESS } from './actionTypes';
+import {
+  LOGIN_START,
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+  SIGNUP_START,
+  SIGNUP_FAILED,
+  SIGNUP_SUCCESS
+} from './actionTypes';
 import { APIUrls } from '../helpers/urls';
 import { getFormBody } from '../helpers/utils';
 
@@ -43,5 +50,55 @@ export function login(email, password) {
         }
         dispatch(loginFailed(data.message));
       });
+  };
+}
+
+
+export function signup(email, password, confirmPassword, name) {
+  return (dispatch) => {
+    const url = APIUrls.signup();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: getFormBody({
+        email,
+        password,
+        confirm_password: confirmPassword,
+        name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log('data', data);
+        if (data.success) {
+          // do something
+          localStorage.setItem('token', data.data.token);
+          dispatch(signupSuccessful(data.data.user));
+          return;
+        }
+        dispatch(signupFailed(data.message));
+      });
+  };
+}
+
+export function startSignup() {
+  return {
+    type: SIGNUP_START,
+  };
+}
+
+export function signupFailed(error) {
+  return {
+    type: SIGNUP_FAILED,
+    error,
+  };
+}
+
+export function signupSuccessful(user) {
+  return {
+    type: SIGNUP_SUCCESS,
+    user,
   };
 }
