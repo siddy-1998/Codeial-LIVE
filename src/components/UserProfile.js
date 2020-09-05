5cbdaa8af1876ce59092c45ab3d87fdff7832029
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-09-05 10:29:08 +0530

    check if user is a friend

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -12,14 +12,33 @@ class UserProfile extends Component {
     }
   }
 
+  checkIfUserIsAFriend = () => {
+    console.log('this.props', this.props);
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.to_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
+
   render() {
-    const { match: { params } , profile } = this.props;
+    const {
+      match: { params },
+      profile,
+    } = this.props;
     console.log('this.props', params);
     const user = profile.user;
 
     if (profile.inProgress) {
       return <h1>Loading...!!!</h1>;
     }
+
+     const isUserAFriend = this.checkIfUserIsAFriend();
 
     return (
       <div className="settings">
@@ -41,16 +60,21 @@ class UserProfile extends Component {
         </div>
 
         <div className="btn-grp">
-          <button className="button save-btn">Add Friend</button>
+          {!isUserAFriend ? (
+            <button className="button save-btn">Add Friend</button>
+          ) : (
+            <button className="button save-btn">Remove Friend</button>
+          )}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ profile }) {
+function mapStateToProps({ profile, friends }) {
   return {
     profile,
+    friends,
   };
 }
 export default connect(mapStateToProps)(UserProfile);
