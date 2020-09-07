0824264a3a50f1429a090a9fb5c229c8dc132f7e
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-09-07 15:14:20 +0530

    Updates

---
+++ precache-manifest.f827522bdb079374ee9576857b50e852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118d71896be9c6bb2bd2c622549c318d",
+    "url": "/Codeial-LIVE/index.html"
+  },
+  {
+    "revision": "f1aa0b94e84fd0af8b5a",
+    "url": "/Codeial-LIVE/static/css/main.df5d6e3a.chunk.css"
+  },
+  {
+    "revision": "6eb9fa7ddfc7bb1ec2c2",
+    "url": "/Codeial-LIVE/static/js/2.75f7e74e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/Codeial-LIVE/static/js/2.75f7e74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1aa0b94e84fd0af8b5a",
+    "url": "/Codeial-LIVE/static/js/main.c396a302.chunk.js"
+  },
+  {
+    "revision": "ca857d5ef0a84675014f",
+    "url": "/Codeial-LIVE/static/js/runtime-main.df812531.js"
+  }
+]);
